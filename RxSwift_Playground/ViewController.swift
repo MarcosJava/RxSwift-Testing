bdc9f0a5cf3818f5e36a0d83cd577a592fb1a388
Marcos Felipe Souza <mfelipesp@gmail.com>
2018-02-16 00:00:29 -0200

    Subscribing to observables ... pag 49

--- RxSwift_Playground/ViewController.swift
+++ RxSwift_Playground/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        example(of: "Marcos") {
+            print("An action")
+        }
+        
+        
+        example(of: "just, of, from") {
+            // 1
+            let one = 1
+            let two = 2
+            let three = 3
+            // 2
+            let observable: Observable<Int> = Observable<Int>.just(one)
+            let observable2 = Observable.of(one, two, three)
+            let observable3 = Observable.of([one, two, three])
+            let observable4 = Observable.from([one, two, three])
+        }
+        
+        
+        let sequence = 0..<3
+        var iterator = sequence.makeIterator()
+        while let n = iterator.next() {
+            print(n)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func example(of description: String, action: () -> Void) {
+        print("\n--- Example of:", description, "---")
+        action()
+    }
+    
+    
+    
 
 
 }
